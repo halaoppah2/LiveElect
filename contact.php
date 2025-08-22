419b47d44c81ca89aeaa5b4aa9ea08b18b2d3f15
halaoppah2 <enochoppah2@gmail.com>
2025-08-22 18:08:30 +0000

    Working on navbar

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
     <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
     <script src="https://kit.fontawesome.com/8161412aed.js" crossorigin="anonymous"></script>
     <!-- <link rel="icon" href="images/logo.png"> -->
-    <title>LiveElect</title>
+    <title>Contact Us</title>
 </head>
 <body>
 
@@ -29,12 +29,12 @@
 
                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
                     <ul class="navbar-nav max-auto text-info">
-                        <li class="nav-item"><a href="index.php" class="nav-link active">Home</a></li>
+                        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
                         <li class="nav-item"><a href="about_us.php" class="nav-link">About US</a></li>
                         <li class="nav-item"><a href="voting.php" class="nav-link">Voting</a></li>
                         <li class="nav-item"><a href="analytics.php" class="nav-link">Analytics Dashboard</a></li>
                         <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
-                        <li class="nav-item"><a href="contact.php" class="nav-link">Contact Us</a></li>
+                        <li class="nav-item"><a href="contact.php" class="nav-link active">Contact Us</a></li>
                     </ul>
               </div>
         </div>
